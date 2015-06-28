3da46084d3373fca35d683a64b2c3d0a3b0e69a0
Isha Soni <isha.soni.18@gmail.com>
2015-06-28 13:21:07 -0500

    added primary and foreign key constraints

--- initial.sql
+++ initial.sql
@@ -3,7 +3,8 @@ CREATE TABLE country
 Country_Name varchar(20),
 Population  decimal(10,2),
 No_of_Worldcup_won int,
-Manager varchar (20)
+Manager varchar (20),
+CONSTRAINT country_pk Primary Key (Country_Name)
 );
 
 CREATE TABLE players
@@ -18,7 +19,9 @@ Height int,
 Club varchar(30),
 Position varchar(10),
 Caps_for_Country int,
-Is_captain varchar(10)
+Is_captain varchar(10),
+CONSTRAINT player_pk Primary Key (Player_id),
+CONSTRAINT CountryFK Foreign Key (Country) REFERENCES country(Country_Name) 
 );
 
 CREATE TABLE match_results
@@ -31,14 +34,18 @@ Team2 varchar(25),
 Team1_score int,
 Team2_score int,
 Stadium_Name varchar(35),
-Host_City varchar(20)
+Host_City varchar(20),
+CONSTRAINT match_pk Primary Key (Match_id),
+CONSTRAINT CountryFK1 Foreign Key (Team1) REFERENCES country(Country_Name),
+CONSTRAINT CountryFK2 Foreign Key (Team2) REFERENCES country(Country_Name) 
 );
 
 CREATE TABLE player_cards
 (
 Player_id int,
 Yellow_Cards int,
-Red_Cards int
+Red_Cards int,
+CONSTRAINT PlayerFK Foreign key (Player_id) REFERENCES players(Player_id) 
 );
 
 CREATE TABLE player_assists_goals 
@@ -47,7 +54,8 @@ Player_id int,
 No_of_Matches int,
 Goals int,
 Assists int,
-Minutes_Played int
+Minutes_Played int,
+CONSTRAINT PlayerFK2 Foreign Key (Player_id) REFERENCES players(Player_id) 
 );
 
 COMMIT;
