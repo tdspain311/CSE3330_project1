ede13f7f58fb66d75d5ef986047c82eb7d1d8daa
Tyler D'Spain <vandyke666@gmail.com>
2015-06-29 22:37:26 -0500

    cleaned up non working loader

--- loadData.php
+++ loadData.php
@@ -11,11 +11,10 @@ if ($conn->connect_error) {
     //die("Connection failed: " . $conn->connect_error);
     echo "Connection error";
 } 
-//else{
-//    echo "Connection successful";
-//
-//}
-//
+
+
+//Country Loader
+
 echo "<br/> <br/> Load Country.csv <br/>";
 
 $myfile = fopen("Input_Data/Country.csv", "r") or die("Unable to openfile!");
@@ -23,13 +22,22 @@ $myfile = fopen("Input_Data/Country.csv", "r") or die("Unable to openfile!");
 while(!feof($myfile)){
     $line = fgets($myfile);
 
-    $newstring = preg_replace("/[\r]/","",$line);
+    $newstring = preg_replace("/[\r\n]/","",$line);
  
-    list($Country_Name, $Population, $No_of_Worldcup_won, $Manager)=explode(",", $newstring);
+    list($Country_Name, 
+		 $Population, 
+		 $No_of_Worldcup_won, 
+		 $Manager)=explode(",", $newstring);
 
     $stmt = $conn->prepare("INSERT INTO `soccer`.`country` (`Country_Name`, `Population`, `No_of_Worldcup_won`, `Manager`) VALUES (?,?,?,?)");
  
-    $stmt->bind_param("sdis", $Country_Name, $Population, $No_of_Worldcup_won, $Manager);
+    $stmt->bind_param("sdis", 
+			$Country_Name, 
+			$Population, 
+			$No_of_Worldcup_won, 
+			$Manager
+	);
+	
     $stmt->execute();
     
     $stmt->close();
@@ -38,33 +46,7 @@ while(!feof($myfile)){
 fflush($myfile);
 
 
-
-//Players.csv
-//echo "<br/> <br/> Load Players.csv <br/>";
-//
-//$myfile = fopen("Input_Data/Players.csv", "r") or die("Unable to openfile!");
-//
-//while(!feof($myfile)){
-//    $line = fgets($myfile);
-//    
-//    $newstring = preg_replace("/[\r]/","",$line);
-//  
-// 
-//    list($Player_id, $Name, $FName, $LName, $DOB, $Country, $Height, $Club, $Position, $Caps_for_Country, $Is_captain)=explode(",", $newstring);
-//
-//    $stmt = $conn->prepare("INSERT INTO `soccer`.`players` (`Player_id', 'Name', 'FName', 'LName', 'DOB', 'Country', 'Height', 'Club', 'Position', 'Caps_for_Country', 'Is_captain`) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
-// 
-//    $stmt->bind_param("isssssissis", $Player_id,$Name,$FName,$LName,$DOB,$Country,$Height,$Club,$Position,$Caps_for_Country,$Is_captain);
-//    $stmt->execute();
-//    
-//    $stmt->close();
-//    
-//}
-//fflush($myfile);
-
-
-
-//Match_results.csv
+//Match Results Loader
 
 echo "<br/> <br/> Load Match_results.csv <br/>";
 
@@ -73,13 +55,32 @@ $myfile = fopen("Input_Data/Match_results.csv", "r") or die("Unable to openfile!
 while(!feof($myfile)){
     $line = fgets($myfile);
 
-    $newstring = preg_replace("/[\r]/","",$line);
+    $newstring = preg_replace("/[\r\n]/","",$line);
  
-    list($Match_id, $Date_of_Match, $Start_Time_Of_Match, $Team1, $Team2, $Team1_score, $Team2_score, $Stadium_Name, $Host_City)=explode(",", $newstring);
-
-    $stmt = $conn->prepare("INSERT INTO `soccer`.`match_results` ('Match_id', 'Date_of_Match', 'Start_Time_Of_Match', 'Team1','Team2','Team1_score','Team2_score','Stadium_Name', 'Host_City') VALUES (?,?,?,?,?,?,?,?,?)");
+    list($Match_id, 
+		 $Date_of_Match, 
+		 $Start_Time_Of_Match, 
+		 $Team1, 
+		 $Team2, 
+		 $Team1_score, 
+		 $Team2_score, 
+		 $Stadium_Name, 
+		 $Host_City)=explode(",", $newstring);
+	
+    $stmt = $conn->prepare("INSERT INTO `soccer`.`match_results` (`Match_id`, `Date_of_Match`, `Start_Time_Of_Match`, `Team1`, `Team2`, `Team1_score`, `Team2_score`, `Stadium_Name`, `Host_City`) VALUES (?,?,?,?,?,?,?,?,?)");
  
-    $stmt->bind_param("issssiiss", $Match_id, $Date_of_Match, $Start_Time_Of_Match, $Team1, $Team2, $Team1_score, $Team2_score, $Stadium_Name, $Host_City);
+    $stmt->bind_param("issssiiss", 
+			$Match_id, 
+			$Date_of_Match, 
+			$Start_Time_Of_Match, 
+			$Team1, 
+			$Team2, 
+			$Team1_score, 
+			$Team2_score, 
+			$Stadium_Name, 
+			$Host_City
+	);
+	
     $stmt->execute();
     
     $stmt->close();
@@ -88,8 +89,7 @@ while(!feof($myfile)){
 fflush($myfile);
 
 
-
-//Player_assists_goals.csv
+//Player Assist Goals Loader
 
 echo "<br/> <br/> Load Player_Assists_Goals.csv <br/>";
 
@@ -97,21 +97,35 @@ $myfile = fopen("Input_Data/Player_Assists_Goals.csv", "r") or die("Unable to op
 
 while(!feof($myfile)){
     $line = fgets($myfile);
-    $newstring = preg_replace("/[\r]/","",$line);
+    $newstring = preg_replace("/[\r\n]/","",$line);
 
-    list($Player_id, $No_of_Matches, $Goals, $Assists, $Minutes_Played)=explode(",", $newstring);
+    list($Player_id, 
+		 $No_of_Matches, 
+		 $Goals, 
+		 $Assists, 
+		 $Minutes_Played)=explode(",", $newstring);
 
   
  $stmt = $conn->prepare("INSERT INTO `soccer`.`player_assists_goals` (`Player_id`, `No_of_Matches`, `Goals`, `Assists`, 'Minutes_Played') VALUES (?,?,?,?,?)");
  
 
-    $stmt->bind_param("iiiii", $Player_id, $No_of_Matches, $Goals, $Assists, $Minutes_Played);
+    $stmt->bind_param("iiiii", 
+			$Player_id, 
+			$No_of_Matches, 
+			$Goals, 
+			$Assists, 
+			$Minutes_Played
+	);
+	
     $stmt->execute();
     
     $stmt->close();
     
 }
 fflush($myfile);
+
+
+//Player Cards Loader
 
 echo "<br/> <br/> Load Player_Cards.csv <br/>";
 
@@ -120,13 +134,20 @@ $myfile = fopen("Input_Data/Player_Cards.csv", "r") or die("Unable to openfile!"
 while(!feof($myfile)){
     $line = fgets($myfile);
 
-    $newstring = preg_replace("/[\r]/","",$line);
+    $newstring = preg_replace("/[\r\n]/","",$line);
  
-    	list($Player_id, $Yellow_Cards, $Red_Cards)=explode(",", $newstring);
+    	list($Player_id, 
+			 $Yellow_Cards, 
+			 $Red_Cards)=explode(",", $newstring);
 
     $stmt = $conn->prepare("INSERT INTO `soccer`.`player_cards` (`Player_id`, `Yellow_Cards`,  `Red_Cards`) VALUES (?,?,?)");
  
-    $stmt->bind_param("iii", $Player_id, $Yellow_Cards, $Red_Cards);
+    $stmt->bind_param("iii", 
+			$Player_id, 
+			$Yellow_Cards, 
+			$Red_Cards
+	);
+	
     $stmt->execute();
     
     $stmt->close();
@@ -134,6 +155,53 @@ while(!feof($myfile)){
 }
 fflush($myfile);
 
+
+//Players Loader
+
+echo "<br/> <br/> Load Players.csv <br/>";
+
+$myfile = fopen("Input_Data/Players.csv", "r") or die("Unable to openfile!");
+
+while(!feof($myfile)){
+    $line = fgets($myfile);
+    
+    $newstring = preg_replace("/[\r\n]/","",$line);
+  
+ 
+    list($Player_id, 
+		 $Name, 
+		 $FName, 
+		 $LName, 
+		 $DOB, 
+		 $Country, 
+		 $Height, 
+		 $Club, 
+		 $Position, 
+		 $Caps_for_Country, 
+		 $Is_captain)=explode(",", $newstring);
+
+    $stmt = $conn->prepare("INSERT INTO `soccer`.`players` (`Player_id', 'Name', 'FName', 'LName', 'DOB', 'Country', 'Height', 'Club', 'Position', 'Caps_for_Country', 'Is_captain`) VALUES (?,?,?,?,?,?,?,?,?,?,?)");
+ 
+    $stmt->bind_param("isssssissis", 
+			$Player_id,
+			$Name,
+			$FName,
+			$LName,
+			$DOB,
+			$Country,
+			$Height,
+			$Club,
+			$Position,
+			$Caps_for_Country,
+			$Is_captain
+	);
+	
+    $stmt->execute();
+    
+    $stmt->close();
+    
+}
+fflush($myfile);
 ?>
 
 
