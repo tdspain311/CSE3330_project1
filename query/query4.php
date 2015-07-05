0cb01ef737b8d045d3bd84f3022fd5242be31417
Tyler D'Spain <vandyke666@gmail.com>
2015-07-05 10:12:59 -0500

    Added 10, 11 and 12 all working, 4 now fixed.. 8 Needs revision

--- query/query4.php
+++ query/query4.php
@@ -7,14 +7,14 @@ $dbname = "soccer";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname); 
 
-$sql = "SELECT Player_id, MAX(Yellow_Cards) AS highYellowCard FROM player_cards;";
+$sql = "SELECT players.Name, players.Country, player_cards.Yellow_Cards FROM players JOIN player_cards ON players.Player_id=player_cards.Player_id WHERE player_cards.Yellow_Cards = (SELECT MAX(player_cards.Yellow_Cards) FROM player_cards);";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    echo "<table border='1'><tr style='text-align: left'><th>Player ID</th><th>Yellow Cards</th></tr>";
+    echo "<table border='1'><tr style='text-align: left'><th>Name</th><th>Country</th><th>Yellow Cards</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["Player_id"]."</td><td>".$row["highYellowCard"]."</td></tr>";
+        echo "<tr><td>".$row["Name"]."</td><td>".$row["Country"]."</td><td>".$row["Yellow_Cards"]."</td></tr>";
     }
     echo "</table>";
 } else {
