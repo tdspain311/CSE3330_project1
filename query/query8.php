564b8d8a238482c80b4f449efa8fecc0de6b4826
Tyler D'Spain <vandyke666@gmail.com>
2015-07-08 19:39:19 -0500

    not a complete fix

--- query/query8.php
+++ query/query8.php
@@ -7,14 +7,21 @@ $dbname = "soccer";
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname); 
 
-$sql = "SELECT Team2, COUNT(Team2) AS Teams, SUM(Team1_score) AS Team1Total, SUM(Team2_score) AS Team2Total FROM match_results GROUP BY Team2;";
+
+// Create View
+$sql = "CREATE VIEW Team_Summary AS SELECT Team1 AS Teams, COUNT(Team1 AND Team2) AS NoOfGames, SUM(Team1_score) AS TotalGoalsFor, SUM(Team2_score) AS TotalGoalsAgainst FROM match_results  WHERE Team2=Team1 GROUP BY Team1;";
+$conn->query($sql);
+
+
+// Query View
+$sql = "SELECT * FROM Team_Summary;";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     echo "<table border='1'><tr style='text-align: left'><th>Team</th><th>Games</th><th>Total Goals Scored</th><th>Total Goals Against</th></tr>";
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        echo "<tr><td>".$row["Team2"]."</td><td>".$row["Teams"]."</td><td>".$row["Team2Total"]."</td><td>".$row["Team1Total"]."</td></tr>";
+        echo "<tr><td>".$row["Teams"]."</td><td>".$row["NoOfGames"]."</td><td>".$row["TotalGoalsFor"]."</td><td>".$row["TotalGoalsAgainst"]."</td></tr>";
     }
     echo "</table>";
 } else {
